144701d17e9c8dafb7566eaca895db2831edcc51
Thamin <github@thamin.ovh>
2020-04-07 08:23:35 +0200

    USe AST module to find imports in python files

--- test_python_imports.py
+++ test_python_imports.py
@@ -1,45 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import ast
 import argparse
-
-
-def clean_import_line(line, pfd):
-    """
-    Get a list of modules imported in current line
-    :param line: line to parse
-    :param pfd: list of python files and directories in project (used to remove local imports)
-    :return: list of imported modules
-    """
-    # If one of those strings is found => line is invalid
-    invalid_strings = ["(", ")", "{", "}", ":"]
-    # If one of those strings is found => remove it from line
-    removable_strings = ["import", " ", "\ufeff"]
-    # If one of those strings is found => only keep left part of line
-    end_line_strings = ["as", ".", "#"]
-    # If end_line_strings in line, take only left part of line
-    for item in end_line_strings:
-        if item in line:
-            line = line.split(item)[0]
-    # Return [] if invalid string in line
-    for item in invalid_strings:
-        if item in line:
-            return []
-    # Remove 'from' from line
-    if line.startswith("from"):
-        line = line.split("from")[1]
-        line = line.split("import")[0]
-    # Remove useless strings from line
-    for item in removable_strings:
-        line = line.replace(item, "")
-    modules = list(set(line.split(",")))
-    for module in modules:
-        # Remove module from list if it is a python file name or a folder name
-        for f in pfd:
-            if f == module:
-                modules.remove(module)
-                break
-    return [module for module in modules if module != ""]
 
 
 def get_directories_in_path(path):
@@ -68,7 +31,31 @@ def get_python_files_in_path(path):
     return set(files)
 
 
-def get_imported_modules(path):
+def get_imports_from_file(path):
+    """
+    Yield every single imports made in a given file
+    :param path: absolute path to the file
+    :return: module name
+    """
+    with open(path) as fh:
+        root = ast.parse(fh.read(), path)
+
+    for node in ast.iter_child_nodes(root):
+        if isinstance(node, ast.Import):
+            module = []
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module.split('.')
+        else:
+            continue
+
+        for n in node.names:
+            if len(module) == 1:
+                yield module[0]
+            else:
+                yield n.name.split('.')[0]
+
+
+def get_imports_from_folder(path):
     """
     Get a list of all python modules imported in path
     :param path: path to use as root
@@ -79,15 +66,14 @@ def get_imported_modules(path):
     for root, dirs, fnames in os.walk(path):
         for fname in fnames:
             if fname.endswith(".py"):
-                with open(os.path.join(root, fname), "r", encoding='utf8') as f:
-                    content = f.read()
-                    lines = content.split("\n")
-                    for line in lines:
-                        # Strip line and clean it from all remaining tabs
-                        line = line.strip().replace("\t", " ")
-                        if "import " in line:
-                            modules = list(set(modules + clean_import_line(line, pfd)))
-    return modules
+                for module in get_imports_from_file(os.path.join(root, fname)):
+                    # Do not add module to list if it is a file / folder name in the project
+                    for f in pfd:
+                        if f == module:
+                            module = None
+                    if module:
+                        modules.append(module)
+    return list(set(modules))
 
 
 def test_import_modules(modules):
@@ -116,7 +102,7 @@ def main(path, dump_success):
     :param dump_success: if True => dump successfully imported modules too
     :return: None
     """
-    modules = get_imported_modules(path)
+    modules = get_imports_from_folder(path)
     success, errors = test_import_modules(modules)
     if dump_success:
         print("SUCCESS:\n{}\n-----".format(success))
